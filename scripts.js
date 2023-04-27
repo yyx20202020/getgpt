651680d355b80a359b4fe831e3d54c8e0067cca4
yyx20202020 <126185233+yyx20202020@users.noreply.github.com>
2023-04-28 01:28:50 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 async function fetchCompanyInfo(companyName) {
-    const response = await fetch(`/api/fetchCompanyInfo?companyName=${encodeURIComponent(companyName)}`);
-    const data = await response.text();
-    return data;
-  }
-  
-  document.getElementById('submit').addEventListener('click', async function() {
-    const companyName = document.getElementById('company-name').value;
-    const companyInfo = await fetchCompanyInfo(companyName);
-    document.getElementById('result').innerText = companyInfo;
-  });
+  const response = await fetch(
+    `/api/fetchCompanyInfo?companyName=${encodeURIComponent(companyName)}`
+  );
+  const data = await response.text();
+  return data;
+}
+
+document.getElementById("company-form").addEventListener("submit", async function (event) {
+  event.preventDefault(); // 阻止表单默认提交行为
+
+  const companyName = document.getElementById("company-name").value;
+  const companyInfo = await fetchCompanyInfo(companyName);
+  document.getElementById("result").innerText = companyInfo;
+});
+
